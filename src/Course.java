345dde83919b19b77f159bafcf2817df0a3a0dc4
Saynasdb <Sayna.sdb@gmail.com>
2024-02-28 20:58:50 +0330

    add custom time and date to course class

--- src/Course.java
+++ src/Course.java
@@ -1,3 +1,6 @@
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.DayOfWeek;
 abstract class Course {
     protected String courseCode;
     protected String courseName;
@@ -11,13 +14,20 @@ abstract class Course {
     protected int examTimeHour;
     protected int examTimeMinute;
     protected int examDuration;
+    protected int courseWeekDay;
+    protected int courseHour;
+    protected int courseMinute;
+    protected int courseDuration;
+    protected DayOfWeek courseDay;
+    protected LocalDateTime examDateTime;
+    protected LocalTime courseTime;
 
-    public Course(String courseCode, String courseName, String professorName, int capacity, int units,
-                  int examTimeYear, int examTimeMonth, int examTimeDay, int examTimeHour, int examTimeMinute,
-                  int examDuration) {
+
+    public Course(String courseCode, String courseName, String professorName, int rawCapacity, int capacity, int units, int examTimeYear, int examTimeMonth, int examTimeDay, int examTimeHour, int examTimeMinute, int examDuration, int courseWeekDay, int courseHour, int courseMinute, int courseDuration) {
         this.courseCode = courseCode;
         this.courseName = courseName;
         this.professorName = professorName;
+        this.rawCapacity = rawCapacity;
         this.capacity = capacity;
         this.units = units;
         this.examTimeYear = examTimeYear;
@@ -26,7 +36,13 @@ abstract class Course {
         this.examTimeHour = examTimeHour;
         this.examTimeMinute = examTimeMinute;
         this.examDuration = examDuration;
-        this.rawCapacity=capacity;
+        this.courseWeekDay = courseWeekDay;
+        this.courseHour = courseHour;
+        this.courseMinute = courseMinute;
+        this.courseDuration = courseDuration;
+        this.examDateTime= LocalDateTime.of(examTimeYear,examTimeMonth,examTimeDay,examTimeHour, examTimeMinute);
+        this.courseTime= LocalTime.of(courseHour, courseMinute);
+        this.courseDay= DayOfWeek.of(courseWeekDay);
     }
 
     public String getCourseCode() {
@@ -65,6 +81,45 @@ abstract class Course {
     public String getExamTime() {
         return examTimeYear + "-" + examTimeMonth + "-" + examTimeDay + " " +
                 examTimeHour + ":" + examTimeMinute + " for " + examDuration + " minutes";
+    }
+    public String getCourseTime() {
+        return courseDay + "-" + courseTime + " for " + courseDuration + " minutes";
+    }
+
+    public int getExamDuration() {
+        return examDuration;
+    }
+
+    public void setExamDuration(int examDuration) {
+        this.examDuration = examDuration;
+    }
+
+    public int getCourseDuration() {
+        return courseDuration;
+    }
+
+    public void setCourseDuration(int courseDuration) {
+        this.courseDuration = courseDuration;
+    }
+
+    public DayOfWeek getCourseDay() {
+        return courseDay;
+    }
+
+    public void setCourseDay(DayOfWeek courseDay) {
+        this.courseDay = courseDay;
+    }
+
+    public LocalDateTime getExamDateTime() {
+        return examDateTime;
+    }
+
+    public void setExamDateTime(LocalDateTime examDateTime) {
+        this.examDateTime = examDateTime;
+    }
+
+    public void setCourseTime(LocalTime courseTime) {
+        this.courseTime = courseTime;
     }
 
     public abstract String getType();
