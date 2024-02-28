a0936360f97d8b64b874ea339ad4268a6fe3bfd0
Saynasdb <Sayna.sdb@gmail.com>
2024-02-28 20:39:45 +0330

    add specializedcourse class

---
+++ src/SpecializedCourse.java
@@ -0,0 +1,17 @@
+class SpecializedCourse extends Course {
+    private String specialization;
+
+    public SpecializedCourse(String courseCode, String courseName, String professorName, int capacity, int units,
+                             int examTimeYear, int examTimeMonth, int examTimeDay, int examTimeHour, int examTimeMinute,
+                             int examDuration) {
+        super(courseCode, courseName, professorName, capacity, units, examTimeYear, examTimeMonth, examTimeDay,
+                examTimeHour, examTimeMinute, examDuration);
+    }
+
+
+
+    @Override
+    public String getType() {
+        return "Specialized";
+    }
+}
