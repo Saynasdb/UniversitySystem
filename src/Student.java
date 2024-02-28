60f858c05b00cbb5d72823c16fd6724d3589d068
Saynasdb <Sayna.sdb@gmail.com>
2024-02-28 21:08:09 +0330

    add check time conflict for course

--- src/Student.java
+++ src/Student.java
@@ -14,4 +14,13 @@ class Student {
     public String getStudentNumber() {
         return studentNumber;
     }
+    private boolean checkTimeConflict(Course course1, Course course2) {
+        //check the day of the week of the course and the time of the course conflict
+        if (course1.courseDay.equals(course2.courseDay)) {
+            if (course1.courseTime.equals(course2.courseTime)) {
+                return true;
+            }
+        }     return false;
+
+    }
     }
