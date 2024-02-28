0870a6f78246b6961ac6ce7891801557a7fee6ea
Saynasdb <Sayna.sdb@gmail.com>
2024-02-28 21:10:56 +0330

    add unregister func

--- src/Student.java
+++ src/Student.java
@@ -14,13 +14,33 @@ class Student {
     public String getStudentNumber() {
         return studentNumber;
     }
+
     private boolean checkTimeConflict(Course course1, Course course2) {
         //check the day of the week of the course and the time of the course conflict
         if (course1.courseDay.equals(course2.courseDay)) {
             if (course1.courseTime.equals(course2.courseTime)) {
                 return true;
             }
-        }     return false;
+        }
+        return false;
 
     }
+    public void unregisterCourse(String courseCode) {
+        for (int i = 0; i < registeredCourses.size(); i++) {
+            if (registeredCourses.get(i).getCourseCode().equals(courseCode)) {
+                Course course = registeredCourses.remove(i);
+                totalUnits -= course.getUnits();
+                if (course.getType().equals("General")) {
+                    generalUnits -= course.getUnits();
+                }
+                course.increaseCapacity();
+                System.out.println("Course " + courseCode + " unregistered successfully.");
+                return;
+            }
+        }
+        System.out.println("Course " + courseCode + " not found in registered courses.");
     }
+
+
+    
+}
