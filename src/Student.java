7e278a0d6650cb163e7010ad9c6de898554ad61d
Saynasdb <Sayna.sdb@gmail.com>
2024-02-28 20:40:42 +0330

    add student class

---
+++ src/Student.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Student {
+    private String studentNumber;
+    private List<Course> registeredCourses = new ArrayList<>();
+    private int totalUnits = 0;
+    private int generalUnits = 0;
+
+    public Student(String studentNumber) {
+        this.studentNumber = studentNumber;
+    }
+
+    
+    }
