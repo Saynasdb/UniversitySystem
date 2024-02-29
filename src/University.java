711a5d4829a19722bbdc0d1e66ddacd462bbdc02
Saynasdb <Sayna.sdb@gmail.com>
2024-02-29 11:29:40 +0330

    add university class

---
+++ src/University.java
@@ -0,0 +1,7 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class University {
+    private List<Faculty> faculties = new ArrayList<>();
+    private List<Student> students = new ArrayList<>();
+}
