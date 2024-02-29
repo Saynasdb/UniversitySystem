cd3a2a62c58e43abcda56d1f0a9f0b8680cb9224
Saynasdb <Sayna.sdb@gmail.com>
2024-02-29 12:59:41 +0330

    add limit check funcs

--- src/Student.java
+++ src/Student.java
@@ -37,6 +37,72 @@ class Student {
         return false;
 
     }
+    private  boolean checkCapacity(Course course){
+        if(course.getCapacity()>0){
+            return true;
+        }
+        System.out.println("Course " + course.getCourseCode() + " is full.");
+        return false;
+    }
+    private boolean checkUnits(Course course){
+        if(course.getUnits()<=21){
+            return true;
+        }
+        System.out.println("Course " + course.getCourseCode() + " exceeds the maximum number of units.");
+        return false;
+    }
+    private boolean checkGeneralUnits(Course course){
+        if(course.getType().equals("General") && generalUnits+course.getUnits()<=5){
+            return true;
+        }
+        if(course.getType().equals("General")){
+            System.out.println("Course " + course.getCourseCode() + " exceeds the maximum number of general units.");
+            return false;
+        }
+        return false;
+    }
+    private boolean checkCourse(Course course){
+        if(checkCapacity(course) && checkUnits(course) && checkGeneralUnits(course)){
+            return true;
+        }
+        return false;
+    }
+    private boolean checkExamTimeConflict(Course course1, Course course2){
+        if(course1.examDateTime.equals(course2.examDateTime)){
+            return true;
+        }
+        return false;
+    }
+public void registerCourse(Course course) {
+
+        if (course != null) {
+            if(checkCourse(course)){
+                for (Course registeredCourse : registeredCourses) {
+                    if (checkTimeConflict(registeredCourse, course)) {
+                        System.out.println("Course " + course.getCourseCode() + " has time conflict with " + registeredCourse.getCourseCode());
+                        return;
+                    }
+                    if(checkExamTimeConflict(registeredCourse, course)){
+                        System.out.println("Course " + course.getCourseCode() + " has exam time conflict with " + registeredCourse.getCourseCode());
+                        return;
+                    }
+                }
+                registeredCourses.add(course);
+                totalUnits += course.getUnits();
+                if (course.getType().equals("General")) {
+                    generalUnits += course.getUnits();
+                }
+                course.decreaseCapacity();
+                System.out.println("Course " + course.getCourseCode() + " registered successfully.");
+            }else{
+                System.out.println("Course " + course.getCourseCode() + " cannot be registered.");
+            }
+        } else {
+            System.out.println("Course " + course.getCourseCode() + " not found.");
+        }
+    }
+
+
     public void unregisterCourse(String courseCode) {
         for (int i = 0; i < registeredCourses.size(); i++) {
             if (registeredCourses.get(i).getCourseCode().equals(courseCode)) {
@@ -52,6 +118,7 @@ class Student {
         }
         System.out.println("Course " + courseCode + " not found in registered courses.");
     }
+
 
 
     public void listRegisteredCourses() {
